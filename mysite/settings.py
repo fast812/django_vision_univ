00a6b796021494ab6902c9d8c5025f15c0d21ea4
fast812 <wizsh23@gmail.com>
2025-12-29 14:29:18 +0900

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', #shc added
-    'pages',
+    'pages', #github commit test
     "notes",
     "aggregator",
 ]
